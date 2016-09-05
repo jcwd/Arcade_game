63f846e632a79c1e717a2924cb44ec83db9e630f
jcwd <jon.collins71@gmail.com>
2016-09-05 09:42:36 +0200

    adds base player class and allEnemies array ( clearing first errors on console.log "allEnemies is not defined and player is not defined")

--- js/app.js
+++ js/app.js
@@ -24,11 +24,30 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function (player) { // this should set up the Player class
+    this.player = player; // keyword this allows me to work on the properties of the player within the class.
+    console.log ("Player instantiated")
 
+};
+//the player method (first just a test to print)
+Player.prototype.hellotest = function() {
+    console.log("hello, I am a test for player " + this.player);
+};
+
+// creating a new instance of the player object
+
+var player = new Player('dave');
+
+// to show the first name properties of the objects
+console.log('player is ' + player.player); // logs "player is dave"
+
+// calling the hellotest function
+player.hellotest();
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var allEnemies = [];
 
 
 
